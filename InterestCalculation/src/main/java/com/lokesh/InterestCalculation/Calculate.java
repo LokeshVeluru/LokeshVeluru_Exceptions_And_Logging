651bcd8fb67399fd8f1ce0e1a0d8828b1da40d5b
LokeshVeluru <lokeshveluru2000@gmail.com>
2020-02-20 16:55:56 +0530

    Initial Commit.......

---
+++ InterestCalculation/src/main/java/com/lokesh/InterestCalculation/Calculate.java
@@ -0,0 +1,9 @@
+package com.lokesh.InterestCalculation;
+
+public interface Calculate {
+	
+	public abstract double calculateSimpleInterest(double principleAmount, double time, double rateOfInterest);
+	
+	public abstract double calculateCompoundInterest(double principleAmount, double time, double rateOfInterest);
+
+}
